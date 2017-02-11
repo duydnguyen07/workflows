89de9e314420febc73823bfdec74e8d4dad0c4d7
DuyNguyen <blinkblinklink@aim.com>
2017-02-10 22:38:07 -0800

    Concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require("gulp"),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 var coffeeSrcs = ['components/coffee/tagline.coffee'];
+var jsSrcs = [
+	'components/scripts/rclick.js',	
+	'components/scripts/pixgrid.js',	
+	'components/scripts/tagline.js',	
+	'components/scripts/template.js',	
+];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSrcs)
 		.pipe(coffee({base: true})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSrcs)
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('builds/development/js'));
 });
